29c66ce0eb780259d55c4eac82d5ad6cbc81d18c
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-12-12 16:58:51 +0100

    add basic express serveur

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const colors = require('colors')
+
+const port = process.env.PORT;
+
+const app = express()
+
+app.listen(port, () => {
+    console.log(`Server started on port : ${port}`)
+})
