489e68982b9d16c9c365831f9cd356061516c21f
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-12-12 23:37:11 +0100

    install express-async-handler

--- backend/controllers/noteControllers.js
+++ backend/controllers/noteControllers.js
@@ -1,15 +1,17 @@
-const getNotes = (req, res) => {
-    res.status(200).json({ message: 'Get notes' })
-}
+const asyncHandler = require('express-async-handler')
 
-const createNote = (req, res) => {
+const getNotes = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Get notes' })
+})
+
+const createNote = asyncHandler(async (req, res) => {
     if (!req.body.title || !req.body.note_content) {
         res.status(400)
         throw new Error('Please add a title or content for your note')
     }
 
     res.status(200).json({ message: 'Create note' })
-}
+})
 
 module.exports = {
     getNotes,
