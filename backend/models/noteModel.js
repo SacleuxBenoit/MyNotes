383a2f96ee00ff1c7cd6fe4d7d03357993567eef
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-12-12 23:58:17 +0100

    create model for note

---
+++ backend/models/noteModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const noteSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please add title value']
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Note', noteSchema)
