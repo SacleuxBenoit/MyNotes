1fc30d38d82328f98a3595de8e0adb4e9246103c
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-12-18 13:42:58 +0100

    user can now logged in

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -43,7 +43,19 @@ const userRegister = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // access Public
 const userLogin = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'User logged in' })
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            username: user.username,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('invalid credentials')
+    }
 })
 
 // @desc Get user
